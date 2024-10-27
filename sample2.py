d6161800ce567b1025626d12c464cfb90a4dcd6d
Serkalem Delelegn <serkalemdelelegn2020@gmail.com>
2024-10-27 20:12:31 +0300

    samples

---
+++ sample2.py
@@ -0,0 +1,21 @@
+friends = 11
+
+# friends = friends + 1
+
+# friends += 1
+
+# friends = friends - 2
+# friends -=2
+# friends = friends * 3
+# friends *= 2
+# friends = friends / 2
+# friends /= 2
+
+# friends= friends ** 2 
+# friends **= 2
+remainder = friends % 2 
+
+print (friends)
+print(remainder)
+
+
---
+++ sample3.py
@@ -0,0 +1,15 @@
+x = 3.14
+y=-4
+z=5
+
+result1 = round(x)
+result2 = abs(y)
+result3 = pow(4,3)
+result4=max(x,y,z)
+result5=min(x,y,z)
+
+print (result1)
+print (result2)
+print (result3)
+print (result4)
+print (result5)
